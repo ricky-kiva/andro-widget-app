1235386d6b25f4c0c15d0e102854a09fdd090128
ricky-kiva <ricky.kiva@gmail.com>
2023-04-15 13:11:02 +0700

    add summary

--- app/src/main/java/com/rickyslash/widgetapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/widgetapp/MainActivity.kt
@@ -9,3 +9,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+// Widget is a component to show information in Android's Home screen
+// Type of widgets:
+// - Information Widgets: shows important information time to time
+// - Collection Widgets: shows many elements with same type (collection of email, collection of images, etc)
+// - Control Widgets: control app's function directly
+// - Hybrid Widgets: Combination of other type of widgets (music widget: shows information & control app)
+
+// Things to consider in making widget:
+// - Widget content: content of the widget
+// - Widget navigation: addition navigation to app
+// - Widget Resizing: add flexibility for user to resize the widget
+// - Layout Considerations: flexibility in screen size
+// - Widget Configuration: Add configuration before displaying the widget
